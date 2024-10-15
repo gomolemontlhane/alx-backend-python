1fabcca737f367dd92d3608d3b97f393be4dbbb3
gomolemontlhane <gomolemontlhane@gmail.com>
2024-10-16 00:43:32 +0200

    fixed pycodestyle errors

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,8 +2,9 @@ import asyncio
 import random
 from typing import AsyncGenerator
 
+
 async def async_generator() -> AsyncGenerator[float, None]:
     """Yields a random number between 0 and 10 every second for 10 seconds."""
-    for _ in range(10):
+    for i in range(10):
         await asyncio.sleep(1)  # Asynchronously wait for 1 second
         yield random.uniform(0, 10)  # Yield a random float between 0 and 10
